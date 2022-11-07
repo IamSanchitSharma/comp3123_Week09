0d4f99f0f125aa406805db1f62f324c147230f31
IamSanchitSharma <sanchit12sharma@gmail.com>
2022-11-06 23:52:44 -0500

    New project added

---
+++ Week09/labweek09/src/App.js
@@ -0,0 +1,30 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo}/>
+        <p> <h1> Welcome to Fullstack Development -1</h1>
+            <h5>React JS Programming Week09 Lab exercise</h5>
+            <h5>101331806</h5>
+              <h5>Sanchit Sharma</h5>
+                <h5>George Brown College,Toronto</h5>
+       
+          
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
